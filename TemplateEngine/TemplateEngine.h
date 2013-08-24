97a4f2ba292f05814a31b364b593c31ce89b9c72
Sasmito Adibowo <adib@basil-salad.com>
2013-08-25 02:33:22 +0800

    Added OS X Static Library.

---
+++ TemplateEngine/TemplateEngine.h
@@ -0,0 +1,13 @@
+//
+//  TemplateEngine.h
+//  TemplateEngine
+//
+//  Created by Sasmito Adibowo on 24-08-13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TemplateEngine : NSObject
+
+@end
---
+++ TemplateEngine/TemplateEngine.m
@@ -0,0 +1,13 @@
+//
+//  TemplateEngine.m
+//  TemplateEngine
+//
+//  Created by Sasmito Adibowo on 24-08-13.
+//
+//
+
+#import "TemplateEngine.h"
+
+@implementation TemplateEngine
+
+@end
